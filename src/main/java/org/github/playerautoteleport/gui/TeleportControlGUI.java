9034a1057ce655ef0df73959a9a6606b809a733c
byMouuse <64748094+byMouuse@users.noreply.github.com>
2022-01-03 22:02:42 +0100

    little fix

--- src/main/java/org/github/playerautoteleport/gui/TeleportControlGUI.java
+++ src/main/java/org/github/playerautoteleport/gui/TeleportControlGUI.java
@@ -35,19 +35,27 @@ public class TeleportControlGUI implements GUIHolder {
 
         ItemStack northItem = new ItemBuilder(Material.PLAYER_HEAD)
                 .setDisplayName("§aBlöcke in Richtung §cNorden")
-                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMWM3Yzk3MmU2Nzg1ZDZiMGFjZWI3NzlhYmRkNzcwMmQ5ODM0MWMyNGMyYTcxZTcwMjkzMGVjYTU4MDU1In19fQ==");
+                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy" +
+                        "5taW5lY3JhZnQubmV0L3RleHR1cmUvMWM3Yzk3MmU2Nzg1ZDZiMGFjZWI3NzlhYmRkNzcwMmQ5ODM0MWMy" +
+                        "NGMyYTcxZTcwMjkzMGVjYTU4MDU1In19fQ==");
 
         ItemStack westItem = new ItemBuilder(Material.PLAYER_HEAD)
                 .setDisplayName("§aBlöcke in Richtung §cWesten")
-                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjdlMTY1YzNlZGM1NTQxZDQ2NTRjNDcyODg3MWU2OTA4ZjYxM2ZjMGVjNDZlODIzYzk2ZWFjODJhYzYyZTYyIn19fQ==");
+                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy" +
+                        "5taW5lY3JhZnQubmV0L3RleHR1cmUvNjdlMTY1YzNlZGM1NTQxZDQ2NTRjNDcyODg3MWU2OTA4ZjYxM2Zj" +
+                        "MGVjNDZlODIzYzk2ZWFjODJhYzYyZTYyIn19fQ==");
 
         ItemStack southItem = new ItemBuilder(Material.PLAYER_HEAD)
                 .setDisplayName("§aBlöcke in Richtung §cSüden")
-                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjY1NzJlNjU1NzI1ZDc4Mzc1YTk4MTdlYjllZThiMzc4MjljYTFmZWE5M2I2MDk1Y2M3YWExOWU1ZWFjIn19fQ==");
+                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy" +
+                        "5taW5lY3JhZnQubmV0L3RleHR1cmUvYjY1NzJlNjU1NzI1ZDc4Mzc1YTk4MTdlYjllZThiMzc4MjljYTFm" +
+                        "ZWE5M2I2MDk1Y2M3YWExOWU1ZWFjIn19fQ==");
 
         ItemStack eastItem = new ItemBuilder(Material.PLAYER_HEAD)
                 .setDisplayName("§aBlöcke in Richtung §cOsten")
-                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTE1N2Q2NWIxOTkyMWM3NjBmZjQ5MTBiMzQwNDQ1NWI5YzJlZTM2YWZjMjAyZDg1MzhiYWVmZWM2NzY5NTMifX19");
+                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy" +
+                        "5taW5lY3JhZnQubmV0L3RleHR1cmUvYTE1N2Q2NWIxOTkyMWM3NjBmZjQ5MTBiMzQwNDQ1NWI5YzJlZTM2" +
+                        "YWZjMjAyZDg1MzhiYWVmZWM2NzY5NTMifX19");
 
         inventory.setItem(11, northItem);
         inventory.setItem(12, westItem);
@@ -56,25 +64,29 @@ public class TeleportControlGUI implements GUIHolder {
 
         ItemBuilder northGreenGlassPaneBuilder = new ItemBuilder(Material.LIME_STAINED_GLASS_PANE)
                 .setDisplayName("§eAnzahl der Blöcke ändern")
-                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksNorth() + " Blöcke Richtung Norden")
+                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksNorth() + " Blöcke Richtung" +
+                        " Norden")
                 .addLoreLine("§f➜ §7§oRechtsklick: Verringern")
                 .addLoreLine("§f➜ §7§oLinksklick: Erhöhen");
 
         ItemBuilder westGreenGlassPaneBuilder = new ItemBuilder(Material.LIME_STAINED_GLASS_PANE)
                 .setDisplayName("§eAnzahl der Blöcke ändern")
-                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksWest() + " Blöcke Richtung Westen")
+                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksWest() + " Blöcke Richtung" +
+                        " Westen")
                 .addLoreLine("§f➜ §7§oRechtsklick: Verringern")
                 .addLoreLine("§f➜ §7§oLinksklick: Erhöhen");
 
         ItemBuilder southGreenGlassPaneBuilder = new ItemBuilder(Material.LIME_STAINED_GLASS_PANE)
                 .setDisplayName("§eAnzahl der Blöcke ändern")
-                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksSouth() + " Blöcke Richtung Süden")
+                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksSouth() + " Blöcke Richtung" +
+                        " Süden")
                 .addLoreLine("§f➜ §7§oRechtsklick: Verringern")
                 .addLoreLine("§f➜ §7§oLinksklick: Erhöhen");
 
         ItemBuilder eastGreenGlassPaneBuilder = new ItemBuilder(Material.LIME_STAINED_GLASS_PANE)
                 .setDisplayName("§eAnzahl der Blöcke ändern")
-                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksEast() + " Blöcke Richtung Osten")
+                .addLoreLine("§f➜ §7" + autoTeleportingPlayer.getAutoWalkSetup().getBlocksEast() + " Blöcke Richtung" +
+                        " Osten")
                 .addLoreLine("§f➜ §7§oRechtsklick: Verringern")
                 .addLoreLine("§f➜ §7§oLinksklick: Erhöhen");
 
@@ -85,7 +97,9 @@ public class TeleportControlGUI implements GUIHolder {
 
         ItemStack startItem = new ItemBuilder(Material.PLAYER_HEAD)
                 .setDisplayName("§eAuto-Teleport starten")
-                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzFlZWFkMWUxN2VjZjUzZjIyMjQ3NWRhZWViYWQzNTI2MTM3Yjc5N2U1Y2I3NjdiOThhYzVlN2ViYjlmZTkifX19");
+                .buildAsCustomSkull(plugin, "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy" +
+                        "5taW5lY3JhZnQubmV0L3RleHR1cmUvMzFlZWFkMWUxN2VjZjUzZjIyMjQ3NWRhZWViYWQzNTI2MTM3Yjc5" +
+                        "N2U1Y2I3NjdiOThhYzVlN2ViYjlmZTkifX19");
 
         inventory.setItem(40, startItem);
 
@@ -132,13 +146,17 @@ public class TeleportControlGUI implements GUIHolder {
                     autoTeleportSetup.addBlockEast();
                 }
             } else if (event.getClick() == ClickType.RIGHT) {
-                if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Norden") && autoTeleportSetup.getBlocksNorth() > 0) {
+                if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Norden")
+                        && autoTeleportSetup.getBlocksNorth() > 0) {
                     autoTeleportSetup.removeBlockNorth();
-                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Westen") && autoTeleportSetup.getBlocksWest() > 0) {
+                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Westen")
+                        && autoTeleportSetup.getBlocksWest() > 0) {
                     autoTeleportSetup.removeBlockWest();
-                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Süden") && autoTeleportSetup.getBlocksSouth() > 0) {
+                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Süden")
+                        && autoTeleportSetup.getBlocksSouth() > 0) {
                     autoTeleportSetup.removeBlockSouth();
-                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Osten") && autoTeleportSetup.getBlocksEast() > 0) {
+                } else if (itemAboveClicked.getItemMeta().getDisplayName().endsWith("Osten")
+                        && autoTeleportSetup.getBlocksEast() > 0) {
                     autoTeleportSetup.removeBlockEast();
                 }
             }
